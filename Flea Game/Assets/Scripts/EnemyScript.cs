c468f22bd26b1335bf4b6c01513640e7aa9d4f63
lfnt <jmccarthy91@gmail.com>
2023-02-22 19:00:02 -0700

    Implemented player (move and attack) and enemies (chase)

---
+++ Flea Game/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public float damage = 20;
+    public float maxHealth = 100;
+    public float moveSpeed = 4f;
+    
+    float currentHealth;
+    Rigidbody2D rigidBody;
+    Transform target;
+    Vector2 moveDirection;
+
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        target = GameObject.Find("Player").transform; // Clean this up later - apparently can target based upon layer?
+    }
+
+    void Update()
+    {
+
+        if (target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            moveDirection = direction;
+            rigidBody.rotation = angle;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(target)
+        {
+            rigidBody.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+        }
+    }
+    
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
---
+++ Flea Game/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public float attackRate = 2f;
+    public int maxHealth = 100;
+    public float attackRange = 0.5f;
+    public float attackDamage = 50f;
+
+    int currentHealth;
+    float nextAttackTime = 0f;
+    
+    public Rigidbody2D rigidBody;
+    public Camera cam;
+
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+
+    Vector2 moveDirection;
+    Vector2 mousePosition;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        //Get movement inputs
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.y = Input.GetAxisRaw("Vertical");
+        moveDirection = moveDirection.normalized;
+
+        //Get rotation inputs
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+
+
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Move!
+        rigidBody.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+
+        //Aim
+        Vector2 lookDirection = mousePosition - rigidBody.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rigidBody.rotation = angle;
+    }
+
+    void Attack()
+    {
+        //swing animation
+        GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 1);
+
+        //check collision
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers); //will want to update this to not be a sphere........
+        
+        //damage
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<EnemyScript>().TakeDamage(attackDamage);
+            Debug.Log("hit enemy!");
+        }
+    }
+
+    void OnDrawGizmos()                                                                     // can delete this later
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+}
